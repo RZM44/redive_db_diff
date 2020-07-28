79695a4030952dd1ad99cd1c8a4eb21ad6289439
serverRobot44 <yefenss@163.com>
2020-07-28 04:04:11 +0000

    202007281022

--- clan_battle_schedule.sql
+++ clan_battle_schedule.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS 'clan_battle_schedule' ('clan_battle_id' INTEGER NOT 
 INSERT INTO clan_battle_schedule VALUES(1001,5,0,1,1,'bgm_M90',1,'2020/05/07 5:00:00','2020/06/02 4:59:59');
 INSERT INTO clan_battle_schedule VALUES(1002,6,1001,1,1,'bgm_M97',2,'2020/06/02 5:00:00','2020/06/30 4:59:59');
 INSERT INTO clan_battle_schedule VALUES(1003,6,1002,1,1,'bgm_M104',1,'2020/06/30 5:00:00','2020/07/28 4:59:59');
-INSERT INTO clan_battle_schedule VALUES(1004,6,1003,1,1,'bgm_M115',2,'2020/07/28 5:00:00','2020/08/25 4:59:59');
+INSERT INTO clan_battle_schedule VALUES(1004,7,1003,1,1,'bgm_M115',2,'2020/07/28 5:00:00','2020/08/25 4:59:59');
 COMMIT;
