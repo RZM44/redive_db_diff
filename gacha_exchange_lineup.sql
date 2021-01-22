c77ed6dac5114e62a4009df73a53d775aae1720a
serverRobot44 <yefenss@163.com>
2021-01-22 06:04:10 +0000

    202101211454

--- gacha_exchange_lineup.sql
+++ gacha_exchange_lineup.sql
@@ -28,4 +28,5 @@ INSERT INTO gacha_exchange_lineup VALUES(24,22,170101,3);
 INSERT INTO gacha_exchange_lineup VALUES(25,23,108601,3);
 INSERT INTO gacha_exchange_lineup VALUES(26,24,108801,3);
 INSERT INTO gacha_exchange_lineup VALUES(27,24,107101,3);
+INSERT INTO gacha_exchange_lineup VALUES(28,25,108801,3);
 COMMIT;
