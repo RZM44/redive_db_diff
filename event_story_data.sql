49ec7fc55bbdf2dd5f609681cd43972085bab7c0
serverRobot44 <yefenss@163.com>
2021-05-04 03:04:10 +0000

    202105011716

--- event_story_data.sql
+++ event_story_data.sql
@@ -10,9 +10,10 @@ INSERT INTO event_story_data VALUES(5006,5,10006,'黑铁的亡灵（梦魇）',0
 INSERT INTO event_story_data VALUES(5007,5,10007,'不给布丁就捣蛋！约定的万圣节派对',0,1,'2020/10/22 15:00:00','2099/12/31 23:59:59');
 INSERT INTO event_story_data VALUES(5008,5,10008,'暮光破坏者',0,1,'2020/11/19 15:00:00','2099/12/31 23:59:59');
 INSERT INTO event_story_data VALUES(5009,5,10009,'忘却的圣歌',0,1,'2020/12/17 15:00:00','2099/12/31 23:59:59');
-INSERT INTO event_story_data VALUES(5010,5,10010,'新春破晓之星大危机！',0,1,'2021/01/14 15:00:00','2099/12/31 23:59:59');
+INSERT INTO event_story_data VALUES(5010,5,10010,'新春破晓之星大危机',0,1,'2021/01/14 15:00:00','2099/12/31 23:59:59');
 INSERT INTO event_story_data VALUES(5011,5,10011,'情人节之战！正中红心的甜蜜战斗',0,1,'2021/02/11 15:00:00','2099/12/31 23:59:59');
-INSERT INTO event_story_data VALUES(5012,5,10012,'王都的名侦探 叹息的追缉者（STALKER）',0,1,'2021/03/11 15:00:00','2099/12/31 23:59:59');
+INSERT INTO event_story_data VALUES(5012,5,10012,'王都的名侦探　叹息的追踪者（STALKER）',0,1,'2021/03/11 15:00:00','2099/12/31 23:59:59');
+INSERT INTO event_story_data VALUES(5013,5,10013,'盛开在阿斯特莱亚的双轮之花',0,1,'2021/05/13 15:00:00','2099/12/31 23:59:59');
 INSERT INTO event_story_data VALUES(5017,5,10017,'Re: 从零开始收集的异世界餐桌',0,1,'2021/04/14 15:00:00','2099/12/31 23:59:59');
 CREATE INDEX 'event_story_data_0_value' on 'event_story_data'('value');
 COMMIT;
