2aa18a2ea5c12992d4d944fabb6f6a2c4d20e140
root <yefenss@163.com>
2023-11-03 18:04:10 +0800

    202311011648

--- unlock_unit_condition.sql
+++ unlock_unit_condition.sql
@@ -163,6 +163,7 @@ INSERT INTO unlock_unit_condition VALUES(117801,'妮侬（万圣节）',1,0,1,2,
 INSERT INTO unlock_unit_condition VALUES(117901,'铃奈（万圣节）',1,0,1,2,31179,145,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2);
 INSERT INTO unlock_unit_condition VALUES(118001,'克蕾琪塔',1,0,1,2,31180,145,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2);
 INSERT INTO unlock_unit_condition VALUES(118501,'花凛',1,0,1,2,31185,15,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2);
+INSERT INTO unlock_unit_condition VALUES(119001,'伊绪（黑暗）',1,0,1,2,31190,145,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2);
 INSERT INTO unlock_unit_condition VALUES(170101,'环奈',1,0,1,2,31701,145,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 INSERT INTO unlock_unit_condition VALUES(170201,'环奈（振袖）',1,0,1,2,31702,145,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 INSERT INTO unlock_unit_condition VALUES(180101,'日和莉（公主）',1,0,1,2,31801,145,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2);
