3c965c5b2c60f2e67a32a8172d7e408145bb7a94
root <yefenss@163.com>
2024-03-11 11:04:11 +0800

    202402281840

--- sqlite_stat1.sql
+++ sqlite_stat1.sql
@@ -157,7 +157,7 @@ INSERT INTO sqlite_stat1 VALUES('shiori_enemy_parameter',NULL,'3769');
 INSERT INTO sqlite_stat1 VALUES('goldset_data',NULL,'70');
 INSERT INTO sqlite_stat1 VALUES('sekai_add_times_data',NULL,'12');
 INSERT INTO sqlite_stat1 VALUES('clan_battle_s_map_data','clan_battle_s_map_data_0_clan_battle_id','186 4');
-INSERT INTO sqlite_stat1 VALUES('unit_unique_equip',NULL,'140');
+INSERT INTO sqlite_stat1 VALUES('unit_unique_equip',NULL,'143');
 INSERT INTO sqlite_stat1 VALUES('story_detail',NULL,'1572');
 INSERT INTO sqlite_stat1 VALUES('content_map_data',NULL,'5');
 INSERT INTO sqlite_stat1 VALUES('odds_name_data',NULL,'197');
